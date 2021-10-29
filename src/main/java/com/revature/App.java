3ca54490170faa616efcf30f47c3e08c80099a07
FeltonMac <feltonmc2@gmail.com>
2021-10-29 02:17:00 -0500

    js work in progress

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,16 +1,32 @@
 package com.revature;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.revature.controllers.Controller;
+import com.revature.controllers.LoginUserController;
+import com.revature.controllers.ReimbursementController;
 import com.revature.controllers.UserController;
+import com.revature.models.Reimbursement;
+import com.revature.models.Role;
+import com.revature.models.Status;
+import com.revature.models.Type;
+import com.revature.models.User;
 
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 public class App {
+	
 	private static Javalin app;
 	public static void main(String[] args) {
-		app = Javalin.create();
+		setDATATABLE();
+		app = Javalin.create((config)->{
+			config.addStaticFiles("/static", Location.CLASSPATH);
+		});
 		
-		configure( new UserController());
+		configure( new UserController(), new ReimbursementController(),new LoginUserController());
 		
 		app.start(8081);
 		
@@ -22,4 +38,23 @@ public class App {
 			c.addRouts(app);
 		}
 	}
+		
+	public static void setDATATABLE() {
+//		this.re_id = re_id;
+//		this.amount = amount;
+//		this.submitted = submitted;
+//		this.resolved = resolved;
+//		this.description = description;
+//		this.author = author;
+//		this.resolver = resolver;
+//		this.status = status;
+//		this.type = type;
+		Reimbursement r = new Reimbursement(200.00, LocalDateTime.now(), LocalDateTime.now(),
+				"first reimb",null,null,new Status("pending"),new Type("lodging"));
+		List<Reimbursement> list=new ArrayList<Reimbursement>();
+		list.add(r);
+			User u = new User("oneone","oneone".hashCode(),"kirby","the star warrior", "starpower@gmail.com",
+					  list,new Role("admin"));
+		}
+	
 }
